952a21345c6421fc3789437f804bec09dd2baec0
Elliot Hesp <elliot.hesp@gmail.com>
2017-01-03 15:13:46 +0000

    Open source firebase-relationship

---
+++ README.md
@@ -0,0 +1,61 @@
+# Firebase Relationship
+[![NPM version][npm-image]][npm-url]
+
+A promise based helper to manage relationships in your Firebase Realtime Database.
+
+## Usage
+
+```bash
+npm install firebase-relationship --save
+```
+
+```javascript
+import Firebase from 'firebase';
+import Relationship from 'firebase-relationship';
+
+// Create a new Firebase & Relationship instance
+const firebase = Firebase.initializeApp({...});
+const relationship = new Relationship('category_product');
+
+// Create a new relationship between a category (id: 123) and a product (id: abc)
+relationship.join(firebase, 123, 'abc');
+
+// Remove the relationship
+relationship.remove(firebase, 123, 'abc');
+```
+
+## API
+
+### constructor(name, path)
+Creates and returns a new relationship.
+
+- [name] Relationship name. Must include only one underscore.
+- [path] Firebase path to store relationships. Defaults to 'relationship'. Must not contain leading or trailing forward slashes.
+
+### join(<Firebase Instance>, leftId, rightId, ?value)
+
+Creates a two way relationship between two IDs. The 4th param is an optional value to store with the relationship (defaults to true).
+
+### remove(<Firebase Instance>, leftId, rightId)
+
+Removes a two way relationship between two IDs.
+
+## Magic Methods
+
+When a new relationship instance is created, two magic methods are available for easy relationship querying. For example, with a "category_product" relationship:
+
+```javascript
+// Get products for a category
+relationship.getCategoryProducts(firebase, categoryId).then((snapshot) => {
+  console.log('Products', snapshot.val());
+});
+
+// Get product categories
+relationship.getProductCategories(firebase, productId).then((snapshot) => {
+  console.log('Categories', snapshot.val());
+});
+```
+
+> Each relationship name is pluralized.
+
+[npm-url]: https://npmjs.org/package/firebase-relationship
---
+++ index.js
@@ -0,0 +1,96 @@
+const pluralize = require('pluralize');
+const namespace = 'firebase-relationship';
+
+function cap(_s) {
+  const s = _s.toLowerCase();
+  return s[0].toUpperCase() + s.slice(1);
+}
+
+class Relationship {
+
+  /**
+   * Relationship name.
+   * @param name
+   * @param path
+   */
+  constructor(name, path = 'relationship') {
+    if (path.startsWith('/') || path.endsWith('/')) {
+      throw new Error(`[${namespace}] Invalid relationship path. Should not start or end with a / (forward slash).`);
+    }
+
+    const parts = name.split('_');
+
+    if (parts.length !== 2) {
+      throw new Error(`[${namespace}] Invalid relationship name. Must contain a single underscore e.g. "category_product"`);
+    }
+
+    const a = [parts[0], pluralize(parts[1])].join('_');
+    const b = [parts[1], pluralize(parts[0])].join('_');
+
+    this._relLeft = [cap(parts[0]), cap(pluralize(parts[1]))].join('');
+    this._relRight = [cap(parts[1]), cap(pluralize(parts[0]))].join('');
+
+    this._leftRef = `/${path}/${a}`;
+    this._rightRef = `/${path}/${b}`;
+
+    // create stub methods i.e getUserMatches & getMatchUsers
+    this[`get${this._relLeft}`] = this._getLeftChildren.bind(this);
+    this[`get${this._relRight}`] = this._getRightChildren.bind(this);
+  }
+
+  /**
+   *
+   * @param firebase
+   * @param aId
+   * @param bId
+   * @param value
+   */
+  join(firebase, aId, bId, value = true) {
+    const db = firebase.database();
+    const _value = value || Date.now();
+    db.ref(`${this._leftRef}/${aId}/${bId}`).set(_value);
+    db.ref(`${this._rightRef}/${bId}/${aId}`).set(_value);
+  }
+
+  /**
+   *
+   * @param firebase
+   * @param aId
+   * @param bId
+   */
+  remove(firebase, aId, bId) {
+    const db = firebase.database();
+    db.ref(`${this._leftRef}/${aId}/${bId}`).remove();
+    db.ref(`${this._rightRef}/${bId}/${aId}`).remove();
+  }
+
+  /**
+   *
+   * @param firebase
+   * @param aId
+   * @returns {Promise}
+   * @private
+   */
+  _getLeftChildren(firebase, aId) {
+    const db = firebase.database();
+    return new Promise((resolve) => {
+      db.ref(`${this._leftRef}/${aId}`).once('value', resolve, this);
+    });
+  }
+
+  /**
+   *
+   * @param firebase
+   * @param bId
+   * @returns {Promise}
+   * @private
+   */
+  _getRightChildren(firebase, bId) {
+    const db = firebase.database();
+    return new Promise((resolve) => {
+      db.ref(`${this._rightRef}/${bId}`).once('value', resolve, this);
+    });
+  }
+}
+
+module.exports = Relationship;
